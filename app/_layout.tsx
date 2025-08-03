be2e8b57de39a37512e8b6b2bc9a68354dde858a
malverneas <malverneas@gmail.com>
2025-08-03 22:39:15 +0200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from '../contexts/AuthContext';
 import { HerdProvider } from '../contexts/HerdContext';
 import { CalfProvider } from '../contexts/CalfContext';
 import { WeightRecordsProvider } from '../contexts/WeightRecordsContext';
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
@@ -25,51 +25,49 @@ export default function RootLayout() {
         <CalfProvider>
           <WeightRecordsProvider>
             <Stack
-            screenOptions={{
-              headerTitleStyle: {
-                fontFamily: 'System',
-                fontSize: 18,
-                fontWeight: '600',
-              },
-              headerShadowVisible: false,
-              headerTintColor: Colors.neutral[900],
-              headerTitleContainerStyle: Platform.select({
-                android: {
-                  paddingTop: androidStatusBarHeight,
+              screenOptions={{
+                headerTitleStyle: {
+                  fontFamily: 'System',
+                  fontSize: 18,
+                  fontWeight: '600',
                 },
-                default: undefined,
-              }),
-              headerStyle: Platform.select({
-                android: {
-                  height: 80 + androidStatusBarHeight,
-                  backgroundColor: Colors.white,
-                },
-                default: {
-                  height: 64,
-                  backgroundColor: Colors.white,
-                },
-              }),
-              contentStyle: Platform.select({
-                android: {
-                  paddingTop: androidStatusBarHeight,
-                  backgroundColor: Colors.neutral[50],
-                },
-                default: {
-                  backgroundColor: Colors.neutral[50],
-                },
-              }),
-            }}
-          >
-            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" options={{ presentation: 'modal' }} />
-          </Stack>
-          <StatusBar style="dark" />
+                headerShadowVisible: false,
+                headerTintColor: Colors.neutral[900],
+                headerTitleContainerStyle: Platform.select({
+                  android: {
+                    paddingTop: androidStatusBarHeight,
+                  },
+                  default: undefined,
+                }),
+                headerStyle: Platform.select({
+                  android: {
+                    height: 80 + androidStatusBarHeight,
+                    backgroundColor: Colors.white,
+                  },
+                  default: {
+                    height: 64,
+                    backgroundColor: Colors.white,
+                  },
+                }),
+                contentStyle: Platform.select({
+                  android: {
+                    paddingTop: androidStatusBarHeight,
+                    backgroundColor: Colors.neutral[50],
+                  },
+                  default: {
+                    backgroundColor: Colors.neutral[50],
+                  },
+                }),
+              }}
+            >
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" options={{ presentation: 'modal' }} />
+            </Stack>
+            <StatusBar style="dark" />
           </WeightRecordsProvider>
         </CalfProvider>
       </HerdProvider>
     </AuthProvider>
   );
-}
-  )
 }
